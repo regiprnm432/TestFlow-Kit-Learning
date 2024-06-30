dada922ee80f01a72cc870438d8b7477a39f04f0
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2024-06-30 07:36:54 +0700

    fix error on component AddStudentDataForm

--- src/components/custom/AddStudentDataForm.tsx
+++ src/components/custom/AddStudentDataForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { 
     Dialog, 
@@ -36,6 +36,7 @@ const AddStudentDataForm = ({
 }: AddStudentFormProps) => {
 
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const form = useForm<FormData>({
         mode: "onBlur",
@@ -50,132 +51,138 @@ const AddStudentDataForm = ({
         }, 3000); // Hide the message after 3 seconds
     };
 
+    useEffect(() => {
+        if (!isDialogOpen) {
+            form.reset();
+            setShowSuccessMessage(false);
+            setErrorMessage("");
+        }
+    }, [isDialogOpen, form]);
+
     return (
-      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <DialogTrigger asChild>
-            <Button
-            className="flex items-center bg-blue-800 text-white py-2 px-4 rounded hover:bg-blue-700"
-            style={{ fontSize: "14px" }}
-            >
-            <FaPlus className="mr-2" />
-            Tambah
-            </Button>
-      </DialogTrigger>
-        <DialogContent className="bg-white p-10 rounded-lg shadow-lg max-w-2xl mx-auto">
-            <DialogHeader>
-                <DialogTitle className="text-lg text-center font-bold mb-4">Form Tambah Data Mahasiswa</DialogTitle>
-            </DialogHeader>
-            <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-                <FormField 
-                control={form.control} 
-                name="nim"
-                rules={{required: "NIM Mahasiswa harus terisi"}}
-                render={({ field, fieldState: { error } }) => (
-                    <FormItem className="flex items-center">
-                    <FormLabel className="w-1/6 flex items-center space-x-2">
-                        <span>Nim</span> 
-                        <span className="text-red-500">*</span>
-                        <span>:</span>
-                    </FormLabel>
-                    <FormControl className="w-5/6">
-                        <Input {...field} className="mt-1 block w-full rounded border-gray-300 bg-gray-50 shadow-sm" />
-                        {error && (
-                            <p className="text-red-600 text-sm mt-1">
-                              {error.message}
-                            </p>
-                        )}
-                    </FormControl>
-                    </FormItem>
-                )}
-                />
-                <FormField 
-                control={form.control} 
-                name="nama"
-                rules={{required: "Nama Mahasiswa harus terisi"}}
-                render={({ field, fieldState: { error } }) => (
-                    <FormItem className="flex items-center">
-                    <FormLabel className="w-1/6 flex items-center space-x-2">
-                        <span>Nama</span> 
-                        <span className="text-red-500">*</span>
-                        <span>:</span>
-                    </FormLabel>
-                    <FormControl className="w-5/6">
-                        <Input {...field} className="mt-1 block w-full rounded border-gray-300 bg-gray-50 shadow-sm" />
-                        {error && (
-                            <p className="text-red-600 text-sm mt-1">
-                              {error.message}
-                            </p>
-                        )}
-                    </FormControl>
-                    </FormItem>
-                )}
-                />
-                <FormField 
-                control={form.control} 
-                name="kelas"
-                rules={{required: "Kelas Mahasiswa harus terisi"}}
-                render={({ field, fieldState: { error } }) => (
-                    <FormItem className="flex items-center">
-                    <FormLabel className="w-1/6 flex items-center space-x-2">
-                        <span>Kelas</span> 
-                        <span className="text-red-500">*</span>
-                        <span>:</span>
-                    </FormLabel>
-                    <FormControl className="w-5/6">
-                        <Input {...field} className="mt-1 block w-full rounded border-gray-300 bg-gray-50 shadow-sm" />
-                        {error && (
-                            <p className="text-red-600 text-sm mt-1">
-                              {error.message}
-                            </p>
-                        )}
-                    </FormControl>
-                    </FormItem>
-                )}
-                />
-                <FormField 
-                control={form.control} 
-                name="prodi"
-                rules={{required: "Prodi Mahasiswa harus terisi"}}
-                render={({ field, fieldState: { error } }) => (
-                    <FormItem className="flex items-center">
-                    <FormLabel className="w-1/6 flex items-center space-x-2">
-                        <span>Prodi</span> 
-                        <span className="text-red-500">*</span>
-                        <span>:</span>
-                    </FormLabel>
-                    <FormControl className="w-5/6">
-                        <Input {...field} className="mt-1 block w-full rounded border-gray-300 bg-gray-50 shadow-sm" />
-                        {error && (
-                            <p className="text-red-600 text-sm mt-1">
-                              {error.message}
-                            </p>
-                        )}
-                    </FormControl>
-                    </FormItem>
-                )}
-                />
-                <div className="flex justify-end space-x-4">
-                <Button type="button" className="bg-transparent border border-blue-800 text-blue-800 rounded-full px-4 py-2 hover:bg-blue-100" onClick={() => setIsDialogOpen(false)}>
-                    Kembali
-                </Button>
-                <Button type="submit" className="bg-blue-800 text-white rounded-full px-4 py-2 hover:bg-blue-700">
-                    Simpan
-                </Button>
-                </div>
-            </form>
-            </Form>
-            {showSuccessMessage && (
-            <div className="absolute inset-x-0 top-0 flex items-center justify-center h-full bg-black bg-opacity-30">
-              <div className="bg-blue-800 p-4 rounded-lg shadow-lg">
-                <p className="text-white font-bold">
-                  Data Mahasiswa Baru Berhasil Disimpan!
-                </p>
-              </div>
-            </div>
-          )}
-        </DialogContent>
-      </Dialog>
+        <>
+            <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                <DialogTrigger asChild>
+                    <Button
+                        className="flex items-center bg-blue-800 text-white py-2 px-4 rounded hover:bg-blue-700"
+                        style={{ fontSize: "14px" }}
+                    >
+                        <FaPlus className="mr-2" />
+                        Tambah
+                    </Button>
+                </DialogTrigger>
+                <DialogContent className="bg-white p-10 rounded-lg shadow-lg max-w-2xl mx-auto">
+                    <DialogHeader>
+                        <DialogTitle className="text-lg text-center font-bold mb-4">Form Tambah Data Mahasiswa</DialogTitle>
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                                <FormField
+                                    control={form.control}
+                                    name="nim"
+                                    rules={{ required: "NIM Mahasiswa harus terisi" }}
+                                    render={({ field, fieldState: { error } }) => (
+                                        <FormItem>
+                                            <FormLabel>Nim</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} className="mt-1 block w-full rounded border-gray-300 bg-gray-50 shadow-sm" />
+                                            </FormControl>
+                                            {error && (
+                                                <p className="text-red-600 text-sm mt-1">
+                                                    {error.message}
+                                                </p>
+                                            )}
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="nama"
+                                    rules={{ required: "Nama Mahasiswa harus terisi" }}
+                                    render={({ field, fieldState: { error } }) => (
+                                        <FormItem>
+                                            <FormLabel>Nama</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} className="mt-1 block w-full rounded border-gray-300 bg-gray-50 shadow-sm" />
+                                            </FormControl>
+                                            {error && (
+                                                <p className="text-red-600 text-sm mt-1">
+                                                    {error.message}
+                                                </p>
+                                            )}
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="kelas"
+                                    rules={{ required: "Kelas Mahasiswa harus terisi" }}
+                                    render={({ field, fieldState: { error } }) => (
+                                        <FormItem>
+                                            <FormLabel>Kelas</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} className="mt-1 block w-full rounded border-gray-300 bg-gray-50 shadow-sm" />
+                                            </FormControl>
+                                            {error && (
+                                                <p className="text-red-600 text-sm mt-1">
+                                                    {error.message}
+                                                </p>
+                                            )}
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="prodi"
+                                    rules={{ required: "Prodi Mahasiswa harus terisi" }}
+                                    render={({ field, fieldState: { error } }) => (
+                                        <FormItem>
+                                            <FormLabel>Prodi</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} className="mt-1 block w-full rounded border-gray-300 bg-gray-50 shadow-sm" />
+                                            </FormControl>
+                                            {error && (
+                                                <p className="text-red-600 text-sm mt-1">
+                                                    {error.message}
+                                                </p>
+                                            )}
+                                        </FormItem>
+                                    )}
+                                />
+                                {errorMessage && (
+                                    <div className="bg-red-100 text-red-700 p-2 mb-4 text-sm">
+                                        {errorMessage}
+                                    </div>
+                                )}
+                                <div className="flex justify-end gap-4">
+                                    <Button
+                                        type="button"
+                                        className="bg-transparent border border-blue-800 text-blue-800 rounded-full px-4 py-2 hover:bg-blue-100"
+                                        onClick={() => setIsDialogOpen(false)}
+                                    >
+                                        Kembali
+                                    </Button>
+                                    <Button
+                                        type="submit"
+                                        className="bg-blue-800 text-white rounded-full px-4 py-2 hover:bg-blue-700"
+                                    >
+                                        Simpan
+                                    </Button>
+                                </div>
+                            </form>
+                        </Form>
+                    </DialogHeader>
+                    {showSuccessMessage && (
+                        <div className="absolute inset-x-0 top-0 flex items-center justify-center h-full bg-black bg-opacity-30">
+                            <div className="bg-blue-800 p-4 rounded-lg shadow-lg">
+                                <p className="text-white font-bold">
+                                    Data Mahasiswa Baru Berhasil Disimpan!
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                </DialogContent>
+            </Dialog>
+        </>
     );
 };
 
